9e42339e073536d02a667eddd43211906a92ff25
しゃってん <schatten4810@gmail.com>
2015-01-03 18:16:51 +0900

    プラグインアクティベータクラス名変更

--- src/main/java/org/dbflute/emecha/eclipse/EMechaPlugin.java
+++ src/main/java/org/dbflute/emecha/eclipse/EMechaPlugin.java
@@ -1,4 +1,4 @@
-package org.dbflute.emecha;
+package org.dbflute.emecha.eclipse;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -6,18 +6,18 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class EMechaPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.dbflute.emecha"; //$NON-NLS-1$
 
 	// The shared instance
-	private static Activator plugin;
+	private static EMechaPlugin plugin;
 	
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public EMechaPlugin() {
 	}
 
 	/*
@@ -43,7 +43,7 @@ public class Activator extends AbstractUIPlugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static EMechaPlugin getDefault() {
 		return plugin;
 	}
 
