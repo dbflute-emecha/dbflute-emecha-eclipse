81de8d95b5f36c2cec094fd33b5b94c9e8745b8e
しゃってん <schatten4810@gmail.com>
2015-01-07 13:08:15 +0900

    Add extension.

---
+++ src/main/java/org/dbflute/emecha/eclipse/sqlassist/EMSQLSourceViewerConfiguration.java
@@ -0,0 +1,13 @@
+/*
+ *
+ */
+package org.dbflute.emecha.eclipse.sqlassist;
+
+import org.eclipse.datatools.sqltools.sqleditor.internal.editor.SQLSourceViewerConfiguration;
+
+/**
+ *
+ */
+public class EMSQLSourceViewerConfiguration extends SQLSourceViewerConfiguration {
+
+}
