64395117d6214b07c8057d5f4bedad093f8ce789
jflute <dbflute@gmail.com>
2015-01-20 17:53:32 +0900

    add source code header license

--- synchronizer/src/main/java/org/dbflute/emecha/eclipse/synchronizer/nls/Messages.java
+++ synchronizer/src/main/java/org/dbflute/emecha/eclipse/synchronizer/nls/Messages.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 the Seasar Foundation and the Others.
+ * Copyright 2014-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
 package org.dbflute.emecha.eclipse.synchronizer.nls;
 
 import org.eclipse.osgi.util.NLS;
